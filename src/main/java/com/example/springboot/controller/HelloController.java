0c41bc57a9739b0d796b57c69d94f9c72c81ecac
justintoblack <870582311@qq.com>
2022-04-22 00:09:58 +0800

    增加了对jsp的支持

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.text.DateFormat;
+import java.util.Date;
+
+@Controller
 public class HelloController {
 
     @RequestMapping("/index")
-    public String sayHello(){
-        return "Hello SpringBoot!!";
+    public String sayHello(Model m){
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        return "hello";
     }
 }
