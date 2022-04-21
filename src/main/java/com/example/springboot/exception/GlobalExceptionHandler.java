a642463bf25cf5550fd170c2a73bf910f65f0664
justintoblack <870582311@qq.com>
2022-04-22 00:35:56 +0800

    增加了对异常的处理

---
+++ src/main/java/com/example/springboot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.springboot.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception exception){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", exception);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("ErrorPage");
+        return mav;
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/ErrorPage.jsp
@@ -0,0 +1,13 @@
+
+<%@ page contentType="text/html;charset=UTF-8"%>
+<html>
+<head>
+    <title>Error Page</title>
+</head>
+<body>
+<div style="border: 1px solid skyblue;padding:50px;">
+    出现了异常：${exception}<br><br>
+    异常URL为：${url}
+</div>
+</body>
+</html>
